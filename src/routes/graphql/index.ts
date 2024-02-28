750dbda92667bbc18b6647631d3a70ae35a2ba64
shoxa <sllx.ok@gmail.com>
2024-02-28 16:09:51 +0500

    feat: feat dephtLimit

--- src/routes/graphql/index.ts
+++ src/routes/graphql/index.ts
@@ -1,6 +1,7 @@
 import { FastifyPluginAsyncTypebox } from '@fastify/type-provider-typebox';
 import { createGqlResponseSchema, gqlResponseSchema, schemaApp } from './schemas.js';
-import { graphql } from 'graphql';
+import { graphql, parse, validate } from 'graphql';
+import depthLimit from 'graphql-depth-limit';
 
 const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
   fastify.route({
@@ -14,6 +15,12 @@ const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
     },
     async handler(req) {
       const { query, variables } = req.body;
+
+      const errors = validate(schemaApp, parse(query), [depthLimit(5)]);
+
+      if (errors.length) {
+        return { errors };
+      }
 
       return await graphql({
         schema: schemaApp,
